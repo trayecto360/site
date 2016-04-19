5137497bbaa8921bdc86af56846d1aaca8cf4a1e
juan.calderon <juan.calderon@gmail.com>
2016-04-19 15:13:00 -0600

    Email original

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -1,14 +1,4 @@
 <?php
-require 'PHPMailer/PHPMailerAutoload.php';
-
-//for using the base_url() function
-$this->load->helper('url');
-
-echo "<script type='javascript/text'>";
-echo "alert('There are no fields to generate a report');"
-echo "window.location.href = '" . base_url() . "admin/ahm/panel';"
-echo "</script>";
-
 // Check for empty fields
 if(empty($_POST['name'])  		||
    empty($_POST['email']) 		||
@@ -19,42 +9,18 @@ if(empty($_POST['name'])  		||
 	echo "No arguments Provided!";
 	return false;
    }
-
+	
 $name = $_POST['name'];
 $email_address = $_POST['email'];
 $phone = $_POST['phone'];
 $message = $_POST['message'];
-
-$mail = new PHPMailer;
-
-$mail->isSMTP();
-$mail->Host = 'smtp.gmail.com';
-$mail->SMTPAuth = true;
-$mail->Username = 'trayecto.360@gmail.com';
-$mail->Password = 'Pest1lozzi';
-$mail->SMTPSecure = 'tls';
-
-$mail->From = $email_address;
-$mail->FromName = $name;
-
-$mail->isHTML(true);
-
-$mail->Subject = 'Test Mail Subject!';
-$mail->Body    = $message;
-
-if(!$mail->send()) {
-    echo 'Message could not be sent.';
-    echo 'Mailer Error: ' . $mail->ErrorInfo;
- } else {
-    echo 'Message has been sent';
-}
+	
 // Create the email and send the message
-//$to = 'trayecto.360@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
-//$email_subject = "Website Contact Form:  $name";
-//$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
-//$headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
-//$headers .= "Reply-To: $email_address";
-//mail($to,$email_subject,$email_body,$headers);
-
-return true;
+$to = 'trayecto.360@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$email_subject = "Website Contact Form:  $name";
+$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
+$headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$headers .= "Reply-To: $email_address";	
+mail($to,$email_subject,$email_body,$headers);
+return true;			
 ?>
